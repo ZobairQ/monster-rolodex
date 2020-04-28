f435d2b30c828458ae210ea74fd90f4084a6a965
Zobair <zobairq@outlook.com>
2020-04-28 21:58:17 +0200

    Updates

---
+++ precache-manifest.cd16ae12a06bfb9a18be1447aab7acab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f710758cf29e3e9787c2bf6b3537fbd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2fca9008441ef7c245bd",
+    "url": "/static/css/main.6e3b8dfe.chunk.css"
+  },
+  {
+    "revision": "8846ff8d8394953c201e",
+    "url": "/static/js/2.090a6926.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.090a6926.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fca9008441ef7c245bd",
+    "url": "/static/js/main.4baca415.chunk.js"
+  },
+  {
+    "revision": "0b26c044026ecf183e79",
+    "url": "/static/js/runtime-main.464fd773.js"
+  }
+]);
